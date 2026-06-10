6669f661023cbf8393ae3f8f6be48cfd1bf5f0d8
Mohammed ehteshamuddin atif <mohammedatif639@gmail.com>
2026-06-11 03:36:51 +0530

    Debug Stripe

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -1,4 +1,4 @@
-import Stripe from "stripe";
+/* import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
 const stripe = new Stripe(
@@ -38,4 +38,61 @@ export async function POST(req: Request) {
   return NextResponse.json({
   url: session.url,
 });
+} */
+
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
+
+const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY!
+);
+
+export async function POST(req: Request) {
+  try {
+    const { cart } = await req.json();
+
+    const session =
+      await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+
+        line_items: cart.map((item: any) => ({
+          price_data: {
+            currency: "aud",
+
+            product_data: {
+              name: item.title,
+            },
+
+            unit_amount: Math.round(item.price * 100),
+          },
+
+          quantity: 1,
+        })),
+
+        mode: "payment",
+
+        success_url:
+          `${process.env.NEXT_PUBLIC_SITE_URL}/success`,
+
+        cancel_url:
+          `${process.env.NEXT_PUBLIC_SITE_URL}/cart`,
+      });
+
+    return NextResponse.json({
+      url: session.url,
+    });
+
+  } catch (error: any) {
+
+    console.error("STRIPE ERROR:", error);
+
+    return NextResponse.json(
+      {
+        error: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
